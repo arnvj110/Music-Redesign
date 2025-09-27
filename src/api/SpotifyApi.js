ee8700d9f8e81a8c5a1dd2c5cd5b5ec1dd564cc8
Arnav Jain <arnavjain92105@gmail.com>
2025-09-27 21:32:05 +0530

    1

--- src/api/SpotifyApi.js
+++ src/api/SpotifyApi.js
@@ -5,7 +5,7 @@ const spotifyClient = axios.create({
   baseURL: 'https://api.spotify.com/v1',
 });
 
-// const VITE_ACCESS_TOKEN = import.meta.env.VITE_ACCESS_TOKEN;
+const VITE_ACCESS_TOKEN = 'BQBwVCn-6aKrFF5skR4vtOTuZ1DSMvCVmIJYYfMHL_37tC-67vz-OgC9UfN-bdH1fDCx4cgT5C9eeGk4BkuLMh9ej1w6AC17sTG46KPetx788f5hwehEK-BgytwadxmGZcfFOYWaDR4';
 
 
 spotifyClient.interceptors.request.use((config) => {
