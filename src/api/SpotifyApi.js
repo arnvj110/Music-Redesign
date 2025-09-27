6a97528d4032f978a0f5bd7e5734c31f068a87c2
Arnav Jain <arnavjain92105@gmail.com>
2025-09-27 18:30:29 +0530

    1

--- src/api/SpotifyApi.js
+++ src/api/SpotifyApi.js
@@ -5,8 +5,10 @@ const spotifyClient = axios.create({
   baseURL: 'https://api.spotify.com/v1',
 });
 
+const VITE_ACCESS_TOKEN = import.meta.env.VITE_ACCESS_TOKEN;
+
 spotifyClient.interceptors.request.use((config) => {
-  config.headers.Authorization = `Bearer ${ACCESS_TOKEN}`;
+  config.headers.Authorization = `Bearer ${VITE_ACCESS_TOKEN}`;
   return config;
 });
 
